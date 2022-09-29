11b83568bc46b4ee85ded0719d13154d69dcf624
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2022-09-29 08:42:44 +0300

    Add files via upload

---
+++ conditions/src/conditions/Main.java
@@ -0,0 +1,24 @@
+package conditions;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 12;
+		
+		if(sayi < 20) {
+			System.out.println("the number is less than 20");
+		
+		}
+			
+		else if(sayi<15) {
+			System.out.println("the number is less than 15");
+		}
+			
+		else {
+			System.out.println("unknown");
+		}
+		
+
+	}
+
+}
